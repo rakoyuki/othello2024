377c482243b455799904cefdb8b107bd6e3a8648
rakoyuki <m2216049tt@ug.jwu.ac.jp>
2024-12-16 16:37:18 +0900

    Update pandai_49.py

--- pandai_49.py
+++ pandai_49.py
@@ -10,3 +10,17 @@ board = [
         [0,0,0,0,0,0],
         [0,0,0,0,0,0],
 ]
+
+!rm -rf othello2024
+!git clone https://github.com/kkuramitsu/othello2024.git
+
+from othello2024.neko64 import nekoAI
+
+!pip install -U kogi-canvas
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from collections import deque
+
+from kogi_canvas import play_othello, PandaAI
